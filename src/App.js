fcaa8a43c3ef6b5315606e99c3a50744e7606d31
Volodymyr Grebeniuk <grebenyukvd@gmail.com>
2024-04-22 12:53:02 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ function App() {
   return (
     <div>
         <header>
-            <p className="text-center font-bold bottom-2 text-emerald-400">Demetra</p>
+            <p className="text-center font-bold bottom-2 text-emerald-400">Demetra1</p>
         </header>
     </div>
   );
