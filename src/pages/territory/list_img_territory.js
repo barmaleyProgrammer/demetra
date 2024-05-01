8d902983b23b358c5832eb5d8fa2e7dfc90ca4f8
Volodymyr Grebeniuk <grebenyukvd@gmail.com>
2024-05-01 10:10:45 +0300

    commit main

--- src/pages/territory/list_img_territory.js
+++ src/pages/territory/list_img_territory.js
@@ -1,7 +1,7 @@
 export default [
     {
         id: 1,
-        image: '/base_image/territory_img/kitchen.jpg'
+        image: '/base_image/territory_img/photo_1.jpg'
     },
     {
         id: 2,
