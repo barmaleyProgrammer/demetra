d505253b5480bdfd756a9c0932a0332719ecfc5f
Volodymyr Grebeniuk <grebenyukvd@gmail.com>
2024-05-06 09:47:15 +0300

    commit

--- src/Components/footer.js
+++ src/Components/footer.js
@@ -1,10 +1,14 @@
+import {NavLink} from "react-router-dom";
+
 const Footer = () => {
 
     return (
         <footer>
             <div className="w-1/2 p-8 text-xs">
                     <div className="text-xs font-light">
-                        <p> © { new Date().getFullYear() } demetra.fish</p>
+                        <NavLink to="/">
+                            <p> © {new Date().getFullYear()} demetra.fish</p>
+                        </NavLink>
                         <p>Всі права захищені</p>
                     </div>
                     <div className="font-light">
